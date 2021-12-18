85b8cdf3e0f5b07ea8ca5a392c5de6e38bb3b701
Grant <grantlee44@gmail.com>
2021-12-17 18:43:07 -0800

    Added in big_room.rb from easy 2

---
+++ rb101_rb109/small_problems/easy_2/big_room.rb
@@ -0,0 +1,19 @@
+SQMETERS_TO_SQFEET = 10.7639
+SQMETERS_TO_SQINCH = 144
+
+puts '==> Enter the length of the room in meters: '
+length = gets.to_f
+
+puts '==> Enter the width of the room in meters: '
+width = gets.to_f
+
+square_meters = (length * width).round(2)
+square_feet = (square_meters * SQMETERS_TO_SQFEET).round(2)
+
+square_inches = (square_feet * SQMETERS_TO_SQINCH).round(2)
+
+puts "The area of the room is #{square_meters} " + \
+     "square meters (#{square_feet} square feet)."
+
+puts "The area of the room is #{square_inches} " + \
+     "square inches."
